d673214f0039c43065860eb9f96d20ea9f5b634f
prunedapazj2246 <45056059+prunedapazj2246@users.noreply.github.com>
2019-02-07 17:04:23 -0800

    Add files via upload

---
+++ src/Remarkable.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str = JOptionPane.showInputDialog("What is your name");
+if(str.equals("Daniel")) {
+JOptionPane.showMessageDialog(null, "You are a master at programming");	
+	}else if(str.equals("Rami")) {
+		JOptionPane.showMessageDialog(null, "You are smart and good at soccer");
+	}else if(str.equals("Joaquin")) {
+		JOptionPane.showMessageDialog(null, "YOU ARE BAD AT EVERYTHING!!!!!");
+		
+	}
+
+	}
+}
