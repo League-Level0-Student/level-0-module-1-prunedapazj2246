0039b36159200a1a7565cde31746abb7aef8e50c
Tacoalarmclock <45108867+Tacoalarmclock@users.noreply.github.com>
2019-02-21 16:10:32 -0800

    finished

---
+++ src/Badgers.java
@@ -0,0 +1,19 @@
+
+public class Badgers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 8; j++) {
+				System.out.println("badger");
+			}
+			for (int k = 0; k < 2; k++) {
+				System.out.println("mushroom");
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			System.out.println("snake");
+		}
+	}
+}
---
+++ src/MyAges.java
@@ -0,0 +1,15 @@
+
+public class MyAges {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 14; i++) {
+			System.out.println(i);
+			if (i <= 12) {
+				System.out.println("My age was " + i);
+			} else {
+				System.out.println("My age is " + i);
+			}
+		}
+	}
+}
